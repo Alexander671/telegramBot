c1507b4f0c73933ab8946ef96039133a8b4b9e5e
Matveev.Alexander <matveevalex97@yandex.ru>
2020-05-08 01:03:05 +0300

    Посмотрели реализацию метода getMe из API telegram

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Network.HTTP.Simple            ( httpBS, getResponseBody )               
+import qualified Data.ByteString.Char8         as BS
 import Lib
 
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    json <- fetchJSON
+    BS.putStrLn json
+
+fetchJSON :: IO BS.ByteString
+fetchJSON = do
+    res <- httpBS "https://api.telegram.org/bot1283054130:AAFMfS1-WADlOtvj77jOm9COzDc8D-JLJIE/getMe"
+    return (getResponseBody res)
