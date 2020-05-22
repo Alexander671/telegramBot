37cebc32021aa9088559c4f256a407ca94171fd1
Matveev.Alexander <matveevalex97@yandex.ru>
2020-05-23 00:59:47 +0300

    На сообщения отвечает, но пока не удаляет из JSON'а Увеличить время ответа + удаление отвеченных сообщений

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,9 @@
 
 module Main where
 
-import           Network.HTTP.Simple            ( httpBS, getResponseBody,parseRequestThrow_ )
+import           Network.HTTP.Simple            ( httpBS, getResponseStatusCode, getResponseBody,parseRequestThrow_,Request )
 import           Data.Aeson  
-import           Data.Text                hiding (zip)
+import           Data.Text                hiding (zip,map)
 import           Data.Aeson.Types          
 import           Control.Monad
 import           GHC.Generics             hiding (from )
@@ -13,37 +13,46 @@ import qualified Data.ByteString.Char8         as BS
 import Lib
 
 token :: String
-token = "1283054130:AAFMfS1-WADlOtvj77jOm9COzDc8D-JLJIE"
+token = undefined
 
-data TelegramFrom = TelegramFrom
+data TelegramChat = TelegramChat
     { chat_message_id :: Int
     
-    } deriving (Show)
+    } deriving (Show,Generic)
 
-instance FromJSON (TelegramFrom) where
-        parseJSON (Object v) = TelegramFrom <$>
+instance FromJSON (TelegramChat) where
+        parseJSON (Object v) = TelegramChat <$>
                                  v .: "id" 
         parseJSON _          = fail "not TelegramFrom"
-instance ToJSON (TelegramFrom) where
-        toJSON (TelegramFrom id) = object ["id" .= id]
+instance ToJSON (TelegramChat) where
 
 
 data TelegramMessage= TelegramMessage
     { message_id :: Int
-    , text :: String
-    , from :: TelegramFrom
+    , chat :: Maybe TelegramChat
+    , text :: Maybe String
     } deriving (Show,Generic)
 
 instance FromJSON (TelegramMessage) where
+        parseJSON (Object v) = TelegramMessage <$>
+                                 v .: "message_id" <*>
+                                 v .:? "from" <*>
+                                 v .:? "text"
+        parseJSON _          = fail "not TelegramMessage"
 instance ToJSON (TelegramMessage) where 
 
 data TelegramResult = TelegramResult
   { update_id :: Int
-  , message :: TelegramMessage
+  , message :: Maybe TelegramMessage
   } deriving (Show,Generic)
 
 instance FromJSON TelegramResult where
+        parseJSON (Object v) = TelegramResult <$>
+                                 v .: "update_id" <*>
+                                 v .:? "message"
+        parseJSON _          = fail "not TelegramFrom"
 instance ToJSON TelegramResult where
+
 
 data TelegramUpdate = TelegramUpdate
   { ok :: Bool
@@ -53,29 +62,52 @@ data TelegramUpdate = TelegramUpdate
 
 
 instance FromJSON (TelegramUpdate) where
+        parseJSON (Object v) = TelegramUpdate <$>
+                                 v .: "ok" <*>
+                                 v .:? "result" <*>
+                                 v .:? "decription" 
+        parseJSON _          = fail "not TelegramUpdate"
 instance ToJSON (TelegramUpdate) where
 
 
-
-main :: IO (Maybe [(Int,String)])
+main :: IO ()
 main = do
     updates <- getUpdates
-    return (getMessage (B.fromStrict updates)) 
+    res <- sequenceA $ putMessage $ getMessage $ B.fromStrict updates 
+    print res
+    main
 
 
 getUpdates :: IO BS.ByteString
-getUpdates = do
+getUpdates  = do
             res <- httpBS url
             return (getResponseBody res)
         where url =parseRequestThrow_ ("https://api.telegram.org/bot" ++ token ++ "/getUpdates")
 
            
-
-getMessage :: B.ByteString -> Maybe [(Int,String)]
+getMessage :: B.ByteString -> Maybe [(Maybe (Maybe Int),Maybe (Maybe String))]
 getMessage id1 = do
-            res         <- decode id1 :: Maybe TelegramUpdate
-            resResult   <- (result res)
-            resMessage  <- return (fmap message resResult)
-            resFrom     <- return (fmap from resMessage)
-            resIdChat   <- return (fmap chat_message_id resFrom)
-            return $ zip resIdChat $ fmap text resMessage
+            res         <- decode id1 :: Maybe TelegramUpdate   
+            resResult   <- result res
+            return $ zip (fmap (fmap (fmap chat_message_id)) (fmap (fmap chat) (mess resResult))) $ fmap (fmap text) $ mess resResult
+            where mess resResult = (fmap message resResult)
+
+putMessage :: Maybe [(Maybe (Maybe Int),Maybe (Maybe String))] -> [IO Int]
+putMessage mes =  requestMessage $ url urlMessage urlChatId
+                  where urlMessage = fmap (map snd) mes
+                        urlChatId  = fmap  (map fst) mes 
+
+url :: Maybe [Maybe (Maybe [Char])] -> Maybe [Maybe (Maybe Int)] -> [Maybe Request]
+url (Just []) _  = []
+url  _ (Just []) = []
+url  _ Nothing = Nothing : []
+url (Just ((Just (Just m)):mc)) (Just ((Just (Just c)):chat)) =Just (parseRequestThrow_ ("https://api.telegram.org/bot" ++ token ++ "/sendMessage?chat_id=" ++ (show c) ++ "&text="++ m )) : url (Just mc) (Just chat)
+
+
+requestMessage :: [Maybe Request] -> [IO Int]
+requestMessage []      = []
+requestMessage (Nothing:req)  = requestMessage req
+requestMessage ((Just r):req) = (do
+                         res  <- httpBS r
+                         return (getResponseStatusCode res)) : requestMessage req
+        
