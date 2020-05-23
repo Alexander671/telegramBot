2cb02e12a700aea1b34ddf36f21b88e82547b14d
Matveev.Alexander <matveevalex97@yandex.ru>
2020-05-23 20:22:33 +0300

    Сделано: увеличенно значение timeout + удаление из запроса результатов, на которые был дан ответ
    Осталось: конфиги + рефакторинг

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import           Network.HTTP.Simple            ( httpBS, getResponseStatusCode, getResponseBody,parseRequestThrow_,Request )
+import           Network.HTTP.Simple            ( httpBS, getResponseBody,parseRequestThrow_,Request )
 import           Data.Aeson  
 import           Data.Text                hiding (zip,map)
 import           Data.Aeson.Types          
@@ -13,7 +13,12 @@ import qualified Data.ByteString.Char8         as BS
 import Lib
 
 token :: String
-token = undefined
+token = "1283054130:AAFMfS1-WADlOtvj77jOm9COzDc8D-JLJIE"
+
+fstTuple  (a,_,_) = a
+sndTuple  (_,a,_) = a
+thirdTuple  (_,_,a) = a
+ 
 
 data TelegramChat = TelegramChat
     { chat_message_id :: Int
@@ -82,32 +87,35 @@ getUpdates :: IO BS.ByteString
 getUpdates  = do
             res <- httpBS url
             return (getResponseBody res)
-        where url =parseRequestThrow_ ("https://api.telegram.org/bot" ++ token ++ "/getUpdates")
+        where url =parseRequestThrow_ ("https://api.telegram.org/bot" ++ token ++ "/getUpdates?timeout=10")
 
            
-getMessage :: B.ByteString -> Maybe [(Maybe (Maybe Int),Maybe (Maybe String))]
+getMessage :: B.ByteString -> Maybe [(Int, Maybe (Maybe Int),Maybe (Maybe String))]
 getMessage id1 = do
             res         <- decode id1 :: Maybe TelegramUpdate   
             resResult   <- result res
-            return $ zip (fmap (fmap (fmap chat_message_id)) (fmap (fmap chat) (mess resResult))) $ fmap (fmap text) $ mess resResult
+            return $ zip3 (fmap update_id resResult) (fmap (fmap (fmap chat_message_id)) (fmap (fmap chat) (mess resResult))) $ fmap (fmap text) $ mess resResult
             where mess resResult = (fmap message resResult)
 
-putMessage :: Maybe [(Maybe (Maybe Int),Maybe (Maybe String))] -> [IO Int]
-putMessage mes =  requestMessage $ url urlMessage urlChatId
-                  where urlMessage = fmap (map snd) mes
-                        urlChatId  = fmap  (map fst) mes 
+putMessage :: Maybe [(Int, Maybe (Maybe Int),Maybe (Maybe String))] -> [IO BS.ByteString]
+putMessage mes =  requestMessage urlUpdate_id $ url urlMessage urlChatId
+                  where urlUpdate_id = fmap (map fstTuple) mes
+                        urlMessage   = fmap (map thirdTuple) mes
+                        urlChatId    = fmap  (map sndTuple) mes 
 
 url :: Maybe [Maybe (Maybe [Char])] -> Maybe [Maybe (Maybe Int)] -> [Maybe Request]
 url (Just []) _  = []
 url  _ (Just []) = []
-url  _ Nothing = Nothing : []
+url  _   Nothing = Nothing : []
+url Nothing  _   = Nothing : []
 url (Just ((Just (Just m)):mc)) (Just ((Just (Just c)):chat)) =Just (parseRequestThrow_ ("https://api.telegram.org/bot" ++ token ++ "/sendMessage?chat_id=" ++ (show c) ++ "&text="++ m )) : url (Just mc) (Just chat)
 
 
-requestMessage :: [Maybe Request] -> [IO Int]
-requestMessage []      = []
-requestMessage (Nothing:req)  = requestMessage req
-requestMessage ((Just r):req) = (do
+requestMessage :: Maybe [Int] -> [Maybe Request] -> [IO BS.ByteString]
+requestMessage (Just [])         _      = []
+requestMessage (Just (x:xs)) (Nothing:req)  = requestMessage (Just xs) req
+requestMessage (Just (x:xs)) ((Just r):req) = (do
                          res  <- httpBS r
-                         return (getResponseStatusCode res)) : requestMessage req
-        
+                         res2 <- httpBS (parseReq x)
+                         return (getResponseBody res2)) : requestMessage (Just xs) req
+                        where parseReq x = parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token ++ "/getUpdates?offset=" ++ show (x+1) ++ "&timeout=10"
