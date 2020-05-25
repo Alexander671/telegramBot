431978019fba268d9a78111ef8cf8c35db73b258
Matveev.Alexander <matveevalex97@yandex.ru>
2020-05-25 05:44:48 +0300

    Сделано: /repeat + /help

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@
 module Main where
 
 import           Network.HTTP.Simple
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS
 import           Data.Aeson  
 import           Data.Text                hiding (zip,map)
 import           Data.Aeson.Types          
@@ -13,15 +15,15 @@ import qualified Data.ByteString.Char8         as BS
 import Lib
 
 token :: String
-token = undefined
+token = "https://api.telegram.org/bot1283054130:AAFMfS1-WADlOtvj77jOm9COzDc8D-JLJIE/"
 
 
 fstTuple  (a,_,_) = a
 sndTuple  (_,a,_) = a
 thirdTuple  (_,_,a) = a
 
-
-
+textRepeat :: String
+textRepeat = "Chhose amount of repeats"
 
 data TelegramKeyBoard = TelegramKeyBoard
      { keyboard :: [[String]]
@@ -102,9 +104,10 @@ main = do
 
 getUpdates :: IO BS.ByteString
 getUpdates  = do
-            res <- httpBS url
-            return (getResponseBody res)
-        where url =parseRequest_ (token ++ "/getUpdates?timeout=10")
+            res1 <- url
+            res  <- httpBS res1
+            return $ getResponseBody res
+        where url =parseRequest (token ++ "getUpdates?timeout=10")
 
            
 getMessage :: B.ByteString -> Maybe [(Int, Maybe (Maybe Int),Maybe (Maybe String))]
@@ -119,31 +122,43 @@ getMessage id1 = do
                   chatid resResult = (((chat_message_id <$>) <$>) <$> (chat <$>) <$> mess resResult)
 
 putMessage :: Maybe [(Int, Maybe (Maybe Int),Maybe (Maybe String))] -> [IO BS.ByteString]
-putMessage mess =  requestMessage urlUpdate_id $ url (urlMessage) urlChatId
+putMessage mess =  url urlUpdate_id urlMessage urlChatId
                   where urlUpdate_id = (map fstTuple) <$> mess
                         urlMessage   = (map thirdTuple) <$> mess
                         urlChatId    = (map sndTuple) <$> mess
 
 
-url :: Maybe [Maybe (Maybe [Char])] -> Maybe [Maybe (Maybe Int)] -> [Maybe Request]
-url (Just []) _  = []
-url  _ (Just []) = []
-url  _   Nothing = Nothing : []
-url Nothing   _  = Nothing : []
-url (Just ((Just (Just m)):mc)) (Just ((Just (Just c)):chat)) 
-                     | m == "/help"   = (Just $ parseRequest_ (token ++ "/sendMessage?chat_id=" ++ (show c) ++ "&text=Маленький помощник")) : url (Just mc) (Just chat) 
-                     | otherwise      = (Just $ parseRequest_ (token ++ "/sendMessage?chat_id=" ++ (show c) ++ "&text="++ m )) : url (Just mc) (Just chat)
+url :: Maybe [Int] -> Maybe [Maybe (Maybe [Char])] -> Maybe [Maybe (Maybe Int)] -> [IO BS.ByteString]
+url (Just [])       _            _  = []
+url   _      Nothing        _  = []
+url (Just (x:xs)) (Just ((Just (Just m)):mc)) (Just ((Just (Just c)):chat)) 
+                     | m == "/help"   = responseMessage x (parseRequest_ (token ++ "sendMessage?chat_id=" ++ (show c) ++ "&text=Маленький помощник")) : url (Just xs) (Just mc) (Just chat) 
+                     
+                     | m == "/repeat" = repeatMessage x : url (Just xs) (Just mc) (Just chat)
+                     | otherwise      = responseMessage x (parseRequest_ (token ++ "sendMessage?chat_id=" ++ (show c) ++ "&text="++ m )) : url (Just xs) (Just mc) (Just chat)
 
 
+deleteUpdate :: Int -> String
+deleteUpdate x = token ++ "getUpdates?offset=" ++ show (x+1) 
 
 
-requestMessage :: Maybe [Int] -> [Maybe Request] -> [IO BS.ByteString]
-requestMessage (Just [])         _      = []
-requestMessage (Just (x:xs)) (Nothing:req)  = requestMessage (Just xs) req
-requestMessage (Just (x:xs)) ((Just r):req) = (do
-                         res  <- httpBS r
-                         res2 <- httpBS (parseReq x)
-                         return (getResponseBody res2)) : requestMessage (Just xs) req
-                        where parseReq x = parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token ++ "/getUpdates?offset=" ++ show (x+1) ++ "&timeout=10"
-
-
+responseMessage :: Int ->  Request -> IO BS.ByteString
+responseMessage iD  x = do
+            res1  <- httpBS x
+            res2  <- httpBS del
+            return (getResponseBody res1)
+            where del = parseRequest_ $ deleteUpdate iD
+         
+repeatMessage ::Int -> IO BS.ByteString
+repeatMessage iD = do
+                request' <- parseRequest $ token
+                let request = request'
+                        { method         = "POST"
+                        , requestBody    = RequestBodyBS $ BS.pack ("{\"method\":\"sendMessage\", \"chat_id\" : " ++ (show 419088070) ++ ",\"text\" : \"" ++ textRepeat ++ "\",\"reply_markup\" : {\"keyboard\" : [[\"1\",\"2\",\"3\",\"4\",\"5\"]]}")  
+                        , requestHeaders = [ ("Content-Type", "application/json; charset=utf-8")]
+                        }   
+                response <- httpBS request
+                musor <- httpBS del
+                return $ getResponseBody response
+                where del = parseRequest_ $ deleteUpdate iD
+          
